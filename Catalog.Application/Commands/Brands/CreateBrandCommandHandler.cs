713d404052c5a876bc3b8d493eb0771ba3572dca
Carlos Núñez <carlosnr3005@gmail.com>
2024-11-26 08:12:09 -0300

    feat(CatalogMappingProfile): added matching attribute mapping

--- Catalog.Application/Commands/Brands/CreateBrandCommandHandler.cs
+++ Catalog.Application/Commands/Brands/CreateBrandCommandHandler.cs
@@ -2,7 +2,6 @@ using Catalog.Application.Dtos;
 using Catalog.Application.Dtos.Entities;
 using Catalog.Application.Exceptions;
 using Catalog.Application.Mappers;
-using Catalog.Domain;
 using Catalog.Domain.Entities;
 using Catalog.Domain.Repositories;
 using Catalog.Domain.Shared;
@@ -15,7 +14,7 @@ public class CreateBrandCommand : IRequest<BaseResponseDto<BrandResponseDto>>
 {
     public string Name { get; }
     public string Description { get; }
-    
+
     public CreateBrandCommand(string name, string description)
     {
         Name = name;
@@ -29,49 +28,33 @@ public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, Bas
     private readonly IUnitOfWork _unitOfWork;
     private readonly ILogger<CreateBrandCommandHandler> _logger;
 
-    public CreateBrandCommandHandler(IBrandRepository brandRepository, IUnitOfWork unitOfWork, ILogger<CreateBrandCommandHandler> logger)
+    public CreateBrandCommandHandler(
+        IBrandRepository brandRepository,
+        IUnitOfWork unitOfWork,
+        ILogger<CreateBrandCommandHandler> logger
+    )
     {
         _brandRepository = brandRepository;
         _unitOfWork = unitOfWork;
         _logger = logger;
     }
 
-    public async Task<BaseResponseDto<BrandResponseDto>> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    public async Task<BaseResponseDto<BrandResponseDto>> Handle(
+        CreateBrandCommand request,
+        CancellationToken cancellationToken
+    )
     {
         // Crear la entidad BrandEntity -> TODO: Mapper de CreateBrandCommand a BrandEntity??
-        BrandEntity brandEntity = new BrandEntity
-        {
-            Name = request.Name,
-            Description = request.Description
-        };
+        BrandEntity brandEntity = CatalogMapper.Mapper.Map<BrandEntity>(request);
         
-        try
-        {
-            await _unitOfWork.BeginTransactionAsync(cancellationToken);
+        // Agregar la nueva marca a la base de datos
+        await _brandRepository.AddWithSaveChange(brandEntity, cancellationToken);
 
-            // Agregar la nueva marca a la base de datos
-            await _brandRepository.AddAsync(brandEntity);
+        if (brandEntity.Id == Guid.Empty) throw new EntityException("Brand not be created. Please try again.");
 
-            // Puedes realizar otras modificaciones aquí si es necesario (segundo posible cambio)
-            // await _inventoryRepository.UpdateStockAsync(request.ProductId, request.Quantity);
-            
-            // Confirmar la transacción
-            await _unitOfWork.CommitAsync(cancellationToken);
-            
-            if (brandEntity.Id == Guid.Empty) throw new EntityException("Brand not be created. Please try again.");
-        }
-        catch (Exception exception)
-        {
-            // La OperationCanceledException no es tratada por defecto como una excepción grave o no controlada, por lo que
-            // puede no llegar al manejador global de excepciones. Deberás capturarla explícitamente en tu código.
-            
-            // Si hay un error, revertir la transacción
-            await _unitOfWork.RollbackAsync();
-            throw;
-        }
+        BaseResponseDto<BrandResponseDto> baseResponseDto = new BaseResponseDto<BrandResponseDto>(
+            CatalogMapper.Mapper.Map<BrandResponseDto>(brandEntity));
 
-        BaseResponseDto<BrandResponseDto> baseResponseDto = new BaseResponseDto<BrandResponseDto>(CatalogMapper.Mapper.Map<BrandResponseDto>(brandEntity));
-        
         return baseResponseDto;
     }
 }
--- Catalog.Application/Mappers/CatalogMappingProfile.cs
+++ Catalog.Application/Mappers/CatalogMappingProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Catalog.Application.Commands.Brands;
 using Catalog.Application.Dtos.Entities;
 using Catalog.Domain.Entities;
 
@@ -22,75 +23,81 @@ public class CatalogMappingProfile : Profile
                 memberOptions => memberOptions.MapFrom(sourceMember => new DateTimeOffset(sourceMember.UpdatedAt).ToUnixTimeMilliseconds())
             );
         
-        CreateMap<BrandEntity, BrandResponseDto>()
-            .IncludeBase<BaseEntity, BrandResponseDto>();
-            // .ForMember(); // TODO: AQUI ME QUEDE, HAY QUE PASAR EL Approval y limitar en la base de datos, quizas con una tabla de estados de aprovacion
+        CreateMap<BrandEntity, BrandResponseDto>().IncludeBase<BaseEntity, BrandResponseDto>();
+        // .ForMember(); // TODO: AQUI ME QUEDE, HAY QUE PASAR EL Approval y limitar en la base de datos, quizas con una tabla de estados de aprovacion
         
-        // CreateMap<ProductEntity, ProductResponseDto>()
-        //     .IncludeBase<BaseEntity, ProductResponseDto>();
+        // Se puede simplificar el mapeo utilizando AutoMapper.
+        // La siguiente configuración es suficiente para mapear las propiedades coincidentes entre CreateBrandCommand y BrandEntity.
+        // Siempre que:
+        //      - Los nombres de las propiedades coincidan (como Name y Description).
+        //      - Las propiedades no coincidentes, como Enabled y Approval, se inicialicen de manera predeterminada en BrandEntity o más adelante en el flujo.
+        CreateMap<CreateBrandCommand, BrandEntity>();
         
-        // CreateMap<BrandEntity, BrandResponseDto>()
-        //     .ForMember(
-        //         destinationMember => destinationMember.Id,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.CreatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => new DateTimeOffset(sourceMember.CreatedAt).ToUnixTimeMilliseconds())
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.UpdatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => new DateTimeOffset(sourceMember.UpdatedAt).ToUnixTimeMilliseconds())
-        //     );
-        //
-        // CreateMap<CategoryEntity, CategoryResponseDto>()
-        //     .ForMember(
-        //         destinationMember => destinationMember.Id,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.CreatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.CreatedAt.Millisecond)
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.UpdatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.UpdatedAt.Millisecond)
-        //     );
-        //
-        // CreateMap<ProductEntity, ProductResponseDto>()
-        //     .ForMember(
-        //         destinationMember => destinationMember.Id,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.CreatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.CreatedAt.Millisecond)
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.UpdatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.UpdatedAt.Millisecond)
-        //     );
-        //
-        // CreateMap<TypeEntity, TypeResponseDto>()
-        //     .ForMember(
-        //         destinationMember => destinationMember.Id,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.CreatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.CreatedAt.Millisecond)
-        //     )
-        //     .ForMember(
-        //         destinationMember => destinationMember.UpdatedAt,
-        //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.UpdatedAt.Millisecond)
-        //     );
+            // CreateMap<ProductEntity, ProductResponseDto>()
+            //     .IncludeBase<BaseEntity, ProductResponseDto>();
 
-        // CreateMap<Product, ProductEntity>();
+            // CreateMap<BrandEntity, BrandResponseDto>()
+            //     .ForMember(
+            //         destinationMember => destinationMember.Id,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.CreatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => new DateTimeOffset(sourceMember.CreatedAt).ToUnixTimeMilliseconds())
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.UpdatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => new DateTimeOffset(sourceMember.UpdatedAt).ToUnixTimeMilliseconds())
+            //     );
+            //
+            // CreateMap<CategoryEntity, CategoryResponseDto>()
+            //     .ForMember(
+            //         destinationMember => destinationMember.Id,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.CreatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.CreatedAt.Millisecond)
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.UpdatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.UpdatedAt.Millisecond)
+            //     );
+            //
+            // CreateMap<ProductEntity, ProductResponseDto>()
+            //     .ForMember(
+            //         destinationMember => destinationMember.Id,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.CreatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.CreatedAt.Millisecond)
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.UpdatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.UpdatedAt.Millisecond)
+            //     );
+            //
+            // CreateMap<TypeEntity, TypeResponseDto>()
+            //     .ForMember(
+            //         destinationMember => destinationMember.Id,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.Id.ToString())
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.CreatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.CreatedAt.Millisecond)
+            //     )
+            //     .ForMember(
+            //         destinationMember => destinationMember.UpdatedAt,
+            //         memberOptions => memberOptions.MapFrom(sourceMember => sourceMember.UpdatedAt.Millisecond)
+            //     );
 
-        // CreateMap<TypeEntity, TypeResponse>().ReverseMap();
-        // CreateMap<BrandEntity, BrandResponse>().ReverseMap(); 
-        // CreateMap<ProductEntity, ProductResponse>().ReverseMap();
-        //
-        // CreateMap<Pagination<Product>, Pagination<ProductResponse>>().ReverseMap();
+            // CreateMap<Product, ProductEntity>();
+
+            // CreateMap<TypeEntity, TypeResponse>().ReverseMap();
+            // CreateMap<BrandEntity, BrandResponse>().ReverseMap(); 
+            // CreateMap<ProductEntity, ProductResponse>().ReverseMap();
+            //
+            // CreateMap<Pagination<Product>, Pagination<ProductResponse>>().ReverseMap();
     }
 }
